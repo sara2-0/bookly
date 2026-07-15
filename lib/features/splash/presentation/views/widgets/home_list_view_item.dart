c33d82d15356f83d8d368f56ebeccc1571942815
sara2-0 <sarahafez888@gmail.com>
2026-07-15 15:29:17 +0300

    add list view to home view

---
+++ lib/features/splash/presentation/views/widgets/home_list_view_item.dart
@@ -0,0 +1,25 @@
+import 'package:bookly_app/core/utiles/assets.dart';
+import 'package:flutter/material.dart';
+
+class HomeListViewItem extends StatelessWidget {
+  const HomeListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: AspectRatio(
+        aspectRatio: 2.7/4,
+        child: Container(
+          height: 100,
+          width: 50,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            image:const DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage(AssetsData.testImage)),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/splash/presentation/views/widgets/home_view_body.dart
+++ lib/features/splash/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/features/splash/presentation/views/widgets/custom_app_bar.dart';
+import 'package:bookly_app/features/splash/presentation/views/widgets/home_list_view_item.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -6,6 +7,26 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: const [CustomAppBar()]);
+    return Column(children: const [CustomAppBar(), HomeListView()]);
   }
 }
+class HomeListView extends StatelessWidget {
+  const HomeListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+       height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context,index){
+        return const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8.0),
+          child: HomeListViewItem(),
+        );
+      }),
+    );
+  }
+}
+
+
