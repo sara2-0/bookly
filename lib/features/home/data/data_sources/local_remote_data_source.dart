f2e2c05808fd4ef729cef195481edde6b25fe6d4
sara2-0 <sarahafez888@gmail.com>
2026-07-19 23:15:36 +0300

    fetch home local data surce

--- lib/features/home/data/data_sources/local_remote_data_source.dart
+++ lib/features/home/data/data_sources/local_remote_data_source.dart
@@ -1,4 +1,6 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
+import 'package:hive/hive.dart';
 
 abstract class HomeLocaleDataSource {
   List<BookEntity> fetchFeaturedBooks();
@@ -8,8 +10,8 @@ abstract class HomeLocaleDataSource {
 class HomeLocalDataSourceImpl extends HomeLocaleDataSource {
   @override
   List<BookEntity> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+    var box = Hive.box<BookEntity>(kFeaturedBooks);
+    return box.values.toList();
   }
 
   @override
