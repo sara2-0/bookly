6280af8d08f4f15c9fccf124fd7a096861ca0795
sara2-0 <sarahafez888@gmail.com>
2026-07-20 15:15:19 +0300

    create server failure from dio error

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,1 +1,46 @@
-class Failure {}
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  final String message;
+  Failure(this.message);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.message);
+  factory ServerFailure.fromDioError(DioException e) {
+    switch (e.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure("Connection timeout with api server");
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send timeout with api server');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Recive timeout with api server');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('Bad certificate with api server');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+          e.response!.statusCode!,
+          e.response!.data,
+        );
+      case DioExceptionType.cancel:
+        return ServerFailure('Request to api server canceled');
+      case DioExceptionType.connectionError:
+        return ServerFailure('No internet connection');
+      case DioExceptionType.unknown:
+        return ServerFailure('There was an error, please try again');
+      case DioExceptionType.transformTimeout:
+        return ServerFailure('Transform timeout with api server');
+    }
+  }
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 404) {
+      return ServerFailure('Your request was not found, please try later');
+    } else if (statusCode == 500) {
+      return ServerFailure('There is a problem with server, please try later');
+    } else if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['data']['message']);
+    } else {
+      return ServerFailure("please try again");
+    }
+  }
+}
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -4,6 +4,7 @@ import 'package:bookly_app/features/home/data/data_sources/local_remote_data_sou
 import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl extends HomeRepo {
   final HomeLocaleDataSource homeLocaleDataSource;
@@ -24,7 +25,10 @@ class HomeRepoImpl extends HomeRepo {
       books = await homeRemoteDataSource.fetchFeaturedBooks();
       return right(books);
     } catch (e) {
-      return Left(Failure());
+      if (e is DioException) {
+        return Left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
     }
   }
 
@@ -39,7 +43,10 @@ class HomeRepoImpl extends HomeRepo {
       books = await homeRemoteDataSource.fetchNewestBooks();
       return right(books);
     } catch (e) {
-      return left(Failure());
+      if (e is DioException) {
+        return Left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
     }
   }
 }
